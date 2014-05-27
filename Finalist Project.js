198a97f2dcb00ab95ef7a4cfb8c21358813b9d70
alrock12 <amy.rockwood@yale.edu>
2014-05-27 16:31:36 -0400

    Tech Bootcamp Project

---
+++ Finalist Project.js
@@ -0,0 +1,16 @@
+
+
+   $(document).ready(function() {
+
+    $("#signup").click(function() {
+        alert("This site is not real for you to sign up. Ideally would redirect through CAS and then to a 'create prpfile page'." );
+    });
+
+    $("#login").click(function() {
+        alert("I know for a fact that you do not have an account to this website. Ideally would redirect you through CAS and then to a 'workspace' page.");
+    });
+
+
+
+    });   
+
